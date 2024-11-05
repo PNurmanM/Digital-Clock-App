22a2022a4c2249a386d6514e6fd84bb9b3df5baf
PNurmanM <nurman767659@gmail.com>
2024-11-05 16:47:11 -0600

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import DigitalClock from "./DigitalClock"
 
 function App() {
-  
+  //"homepage": "http://PNurmanM.github.io/Digital-Clock-App",
   return (
     <>
-
+      <DigitalClock/>
     </>
   )
 }
--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -5,8 +5,12 @@ function DigitalClock(){
     const [time, setTime] = useState(new Date());
 
     useEffect(() => {
+        document.title = "Digital Clock"
+    }, []);
+
+    useEffect(() => {
         const timeInterval = setInterval(() => {
-            setTime(newDate());
+            setTime(new Date());
         }, 1000)
 
         return () => {
@@ -20,17 +24,40 @@ function DigitalClock(){
         const s = t.getSeconds();
         const AP = h > 12 ? "PM" : "AM";
 
-        return h + ':' + m + ':' + s + AP;
+        h = h % 12 || 12;
+
+        return padZero(h) + ':' + padZero(m) + ':' + padZero(s) + ' ' + AP;
+    }
+
+    function getDtYr(t){
+        const match = {0:"January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June", 6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December"};
+        const y = t.getFullYear();
+        const mt = t.getMonth();
+        const d = t.getDate();
+
+        return match[mt] + ' ' + d + ', ' + y;
+
+    }
+
+    function padZero(t){
+        return(t < 10 ? "0" + t : t)
     }
 
     return(
-        <div className='clockCont'>
-            <div className='clock'>
-                <span className='time'>
-                    {formatTime(time)}
-                </span>
+        <>
+            <div className='container'>
+                <div className='clockCont'>
+                    <div className='clock'>
+                        <span className='time'>
+                            {formatTime(time)}
+                        </span>
+                    </div>      
+                </div>
+                <div className='dateCont'>
+                <p className='date'>{getDtYr(time)}</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
